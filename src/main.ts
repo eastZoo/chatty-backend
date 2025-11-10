b441b18fc4d003e0cfd81bd1b4acd3002243f5ed
eastZoo <dongju010@naver.com>
2025-11-10 22:39:25 +0900

    UPDATE : 젠킨스 테스트 #2

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,9 @@ async function bootstrap() {
 
   const port = configService.get<number>('PORT') || 3000;
   await app.listen(port);
-  console.log(`Application is running on: http://localhost:${port}`);
+  console.log(
+    `Application is running on ver 1.11.10: http://localhost:${port}`,
+  );
 }
 
 bootstrap();
