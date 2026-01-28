5cdde56e7b9925eea196eeced1ecb27f9f4b1ff4
eastzoo <dongju010@insystem.kr>
2026-01-28 10:34:47 +0900

    UPDATE : all backup

--- src/firebase/firebase.module.ts
+++
@@ -1,8 +0,0 @@
-import { Module } from '@nestjs/common';
-import { FirebaseAdminProvider } from './firebase-admin';
-
-@Module({
-  providers: [FirebaseAdminProvider],
-  exports: ['FIREBASE_ADMIN'],
-})
-export class FirebaseModule {}
