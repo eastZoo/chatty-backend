0b0d89a26934ff96caae03d1a2b56bea712f16e1
JIHYE KIM <kimjeje15@gmail.com>
2026-02-03 19:29:44 +0900

    UPDATE : 메세지 읽음 표시 추가

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -14,6 +14,7 @@ import { AuthModule } from 'src/auth/auth.module';
 import { ConfigModule } from '@nestjs/config';
 import { FcmToken } from 'src/entities/fcm-token.entity';
 import { FirebaseModule } from 'src/firebase/firebase.module';
+import { MessageReadStatus } from 'src/entities/message-read-status.entity';
 
 @Module({
   imports: [
@@ -24,6 +25,7 @@ import { FirebaseModule } from 'src/firebase/firebase.module';
       Users,
       ChatReadStatus,
       FcmToken,
+      MessageReadStatus,
     ]),
     FirebaseModule,
     forwardRef(() => FilesModule),
